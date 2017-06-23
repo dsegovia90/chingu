be9057a1958ef8c35df5931534b8d656e2702f51
Daniel Segovia <daniel@dabici.mx>
2017-06-23 13:51:23 -0500

    added isLoggedIn function for future use and res.locals.user for use in this user session

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,31 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport')
 
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next()
+  }else{
+    res.redirect('/')
+  }
+}
+
+router.use(function(req, res, next){
+  res.locals.user = req.user
+  next()
+})
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Chingu PP', slack_id: process.env.SLACK_CLIENT_ID });
 });
 
 /* Start slack OAuth flow */
-router.get('/auth/slack', passport.authorize('slack'));
+router.get('/auth/slack', passport.authenticate('slack'));
 
 /* Slack OAuth callback url */
 router.get('/auth/slack/callback', 
-  passport.authorize('slack', { failureRedirect: '/login' }),
+  passport.authenticate('slack', { failureRedirect: '/login' }),
   (req, res) => {
-    console.log(req.body)
     res.redirect('/')
   }
 );
