28a3343abea6c620958d0f46aefb2725384774af
Daniel Segovia <daniel@dabici.mx>
2017-06-23 11:18:12 -0500

    passing the client id to view (not recommended)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Chingu PP' });
+  res.render('index', { title: 'Chingu PP', slack_id: process.env.SLACK_CLIENT_ID });
 });
 
 module.exports = router;
