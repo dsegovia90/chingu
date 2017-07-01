578f28b72a77d471b8b660ec790953bede85b949
AgathaLynn <amaskelton@verizon.net>
2017-06-30 20:42:15 -0400

    added semicolons

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport')
+var passport = require('passport');
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
-    return next()
+    return next();
   } else {
-    res.redirect('/')
+    res.redirect('/');
   }
 }
 
 router.use(function (req, res, next) {
-  res.locals.user = req.user
-  next()
-})
+  res.locals.user = req.user;
+  next();
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -27,13 +27,13 @@ router.get('/auth/slack', passport.authenticate('slack'));
 router.get('/auth/slack/callback',
   passport.authenticate('slack', { failureRedirect: '/login' }),
   (req, res) => {
-    res.redirect('/')
+    res.redirect('/');
   }
 );
 
 router.get('/logout', isLoggedIn, function (req, res) {
-  req.logout()
-  res.redirect('/')
-})
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
