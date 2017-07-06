f04ef908995819526fe686061ca70c47aaf46733
Daniel Segovia <daniel@dabici.mx>
2017-07-05 23:16:50 -0500

    commented out the request-form-button function and made an event lister to listen for any changes in the select element, it sends an ajax PUT to /update-user route

--- public/main.js
+++ public/main.js
@@ -1,4 +1,19 @@
-document.getElementById("request-form-button").addEventListener("click", function() {
-  console.log('clicked');
-  document.getElementById("request-form").style.display = "block";
+// document.getElementById("request-form-button").addEventListener("click", function() {
+//   console.log('clicked');
+//   document.getElementById("request-form").style.display = "block";
+// });
+document.getElementById('timezone-select').addEventListener('change', function(e){
+  console.log(e.target.value);
+  
+  var xhr = new XMLHttpRequest();
+    xhr.open('PUT', '/update-user');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var userInfo = JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send(JSON.stringify({
+        newTimeZone: e.target.value
+    }));
 });
