f0d7212a5f3a0e8725f617ab56ebe2264a127c59
Daniel Segovia <daniel@dabici.mx>
2017-07-06 10:28:30 -0500

    exported router.put /update-timezone callback function to separate file

--- routes/index.js
+++ routes/index.js
@@ -33,15 +33,7 @@ router.get('/login', function(req, res) {
 router.use('/auth', require('./auth'));
 
 /* Handle ajax request to change timezone */
-router.put('/update-user', isLoggedIn, function(req, res){
-  console.log(req.body.newTimeZone); //this attribute is sent by the ajax request
-  User.findOne({_id: req.user._id}, function(err, user){
-    user.pending.timezone = req.body.newTimeZone;
-    user.save();
-  });
-  //I would love to send a message back to the app upon successfull change of the timezone @dsegovia90
-  res.json({received: true})
-});
+router.put('/update-timezone', isLoggedIn, require('./update-timezone'));
 
 /* Handle form submission - create/update request for partner */
 router.use('/create-request', isLoggedIn, require('./create-request'));
