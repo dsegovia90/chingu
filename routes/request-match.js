a44cb614b8bb70988c9fda03fdf2dedb1337da28
Daniel Segovia <daniel@dabici.mx>
2017-07-08 18:48:43 -0500

    handling the get request and post request

--- routes/request-match.js
+++ routes/request-match.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users')
 
-router.get('/', function(req, res) {
-  res.render('request-match')
+router.get('/', function (req, res) {
+  res.render('request-match');
 });
 
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
+  var timezone = req.body.timezone;
+  var fccScore = req.body.fccScore;
 
+  User.findOne({ _id: req.user._id }, function (err, user) {
+    console.log(user.pending.created)
+    if (user.pending.created) {
+      user.pending.created = new Date();
+      user.pending.timezone = timezone;
+      user.pending.level = fccScore;
+      user.save()
+    } else {
+      user.pending.created = new Date();
+      user.pending.timezone = timezone;
+      user.pending.level = fccScore;
+      user.save()
+    }
+  })
+  res.redirect('/')
 })
 
 module.exports = router;
