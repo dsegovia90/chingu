9c7ced350f617bc0696dfd014d0fe62a3503a46c
Daniel Segovia <daniel@dabici.mx>
2017-07-08 17:13:11 -0500

    comments explaning each function, and general re-arrangement of the functions

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var passport = require('passport');
 var User = require('../models/users');
 
+
+/*  Checks if the user is authenticated, 
+    it can be used to redirect to /login unauthenticated 
+    access to certain routes. */
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -11,6 +15,10 @@ function isLoggedIn(req, res, next) {
   }
 }
 
+/*  Send the req.user to the view as user variable.
+    Even if the user is undefined (this can be used to
+    show or hide data depending if there is a user 
+    present or not). */
 router.use(function (req, res, next) {
   res.locals.user = req.user;
   next();
@@ -23,27 +31,29 @@ router.get('/', isLoggedIn, function (req, res, next) {
   });
 });
 
+/* Landing Page! (where users log in via slack) */
 router.get('/login', function (req, res) {
   res.render('index', {
     title: 'Chingu PP'
   });
 });
 
-/* Handle slack OAuth process */
-router.use('/auth', require('./auth'));
+/* Logs user out */
+router.get('/logout', isLoggedIn, function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 /* Handle ajax request to change timezone */
 router.put('/update-timezone', isLoggedIn, require('./update-timezone'));
+
+/* Handle slack OAuth process */
+router.use('/auth', require('./auth'));
 
 /* Handle form submission - create/update request for partner */
 router.use('/create-request', isLoggedIn, require('./create-request'));
 
 /* Handle form submission - cancel request for partner */
 router.use('/cancel-request', isLoggedIn, require('./cancel-request'));
-
-router.get('/logout', isLoggedIn, function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
 
 module.exports = router;
