209edb6231635d1d2acc0a4b60fab46615cef3a2
Daniel Segovia <daniel@dabici.mx>
2017-07-10 15:08:10 -0500

    added the admin to false by default

--- config/passport.js
+++ config/passport.js
@@ -1,41 +1,42 @@
-var SlackStrategy = require('passport-slack').Strategy
-var User = require('../models/users')
+var SlackStrategy = require('passport-slack').Strategy;
+var User = require('../models/users');
 
 module.exports = function(passport){
   passport.serializeUser(function(user, done){
-		done(null, user.id)
+		done(null, user.id);
 	})
 	passport.deserializeUser(function(id, done){
 		User.findById(id, function(err, user){
-			done(err, user)
+			done(err, user);
 		})
 	})
 
   passport.use(new SlackStrategy({
-		clientID: process.env.SLACK_CLIENT_ID,
-		clientSecret: process.env.SLACK_CLIENT_SECRET
+		clientID: process.env.SLACK_CLIENT_ID,;
+		clientSecret: process.env.SLACK_CLIENT_SECRET;
 	},
 	function(token, refreshToken, profile, done) {
 		process.nextTick(function(){
 			User.findOne({ 'slack.id': profile.id }, function(err, user){
 				if(err){
-					return done(err)
+					return done(err);
 				}
 
 				if(user){
-					return done(null, user)
+					return done(null, user);
 				}else{
-					var newUser = new User()
+					var newUser = new User();
 
-					newUser.slack.id = profile.user.id
-					newUser.slack.displayName = profile.user.name
-					newUser.slack.email = profile.user.email
-					newUser.slack.image = profile.user.image_1024
-					newUser.slack.team = profile.team
+					newUser.slack.id = profile.user.id;
+					newUser.slack.displayName = profile.user.name;
+					newUser.slack.email = profile.user.email;
+					newUser.slack.image = profile.user.image_1024;
+					newUser.slack.team = profile.team;
+					newUser.admin = false;
 
 					newUser.save(function(err){
 						if(err) throw err
-            return done(null, newUser)
+            return done(null, newUser);
 					})
 				}
 			})
