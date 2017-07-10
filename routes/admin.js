72292f5128a05532f96ea9109c4b1f3b1bf3dad7
Daniel Segovia <daniel@dabici.mx>
2017-07-10 15:31:03 -0500

    checking if user is logged in and admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+function isLoggedInAndAdmin(req, res, next) {
+  if (req.isAuthenticated() && req.user.admin) {
+    return next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
 /* GET users listing. */
-router.get('/users', function (req, res, next) {
-  res.render('index', { title: 'Chingu!' })
+router.get('/', isLoggedInAndAdmin, function (req, res, next) {
+  res.render('admin')
 });
 
 module.exports = router;
