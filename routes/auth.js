a1f2bdb6450f6adf0db4befaba4c1053b96b46cc
Daniel Segovia <daniel@dabici.mx>
2017-07-11 15:42:50 -0500

    handling the logic of the slack app install

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var slack = require('slack')
+var Team = require('../models/teams.js')
 
 /* Start slack OAuth flow */
 router.get('/slack', passport.authenticate('slack'));
@@ -12,5 +14,20 @@ router.get('/slack/callback',
     res.redirect('/');
   }
 );
+
+router.get('/slack/install', function (req, res) {
+  var client_id = process.env.SLACK_CLIENT_ID;
+  var client_secret = process.env.SLACK_CLIENT_SECRET;
+  var code = req.query.code;
+  var redirect_uri = 'http://localhost:3000/auth/slack/install'
+  slack.oauth.access({ client_id, client_secret, code, redirect_uri }, (err, data) => {
+    if(err){
+      console.error(err);
+    }
+    console.log(data)
+    
+    res.redirect('/login')
+  })
+})
 
 module.exports = router;
