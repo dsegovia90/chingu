13bbed57a0879cf63059ad22f148efe4b7589462
Daniel Segovia <daniel@dabici.mx>
2017-07-11 12:12:17 -0500

    Date.now changed to new Date()

--- models/matches.js
+++ models/matches.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var Match = new Schema({
   users: [{type: Schema.Types.ObjectId, ref: 'User'}],
-  created: { type: Date, default: Date.now }
+  created: { type: Date, default: new Date() }
 });
 
 module.exports = mongoose.model('Match', Match);
