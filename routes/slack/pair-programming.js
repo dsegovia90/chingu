5c7fb085dc46ead7d8b2a90d6e21a064343d4610
AgathaLynn <amaskelton@verizon.net>
2017-07-16 17:33:10 -0400

    added handler for /pair-programming slack command

--- routes/slack/pair-programming.js
+++ routes/slack/pair-programming.js
@@ -1,8 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-router.get('/', function(req, res) {
+const router = express.Router();
+
+const User = require('../../models/users');
+
+router.get('/', function (req, res) {
   res.send('Here I am - You called me?');
+});
+
+router.post('/', function (req, res) {
+  // check verification token
+  if (req.body.token !== process.env.VERIFICATION_TOKEN) {
+    return;
+  }
+
+  // find user in database
+  User.findOne({ 'slack.id': req.body.user_id })
+  .then(
+    function resolved (user) {
+      if (user) {
+        res.json({
+          text: 'Hi, there - I know you!',
+        });
+      } else {
+        res.json({
+          text: 'A newbie!',
+        });
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.json({text: 'error'});
+    });
 });
 
 module.exports = router;
