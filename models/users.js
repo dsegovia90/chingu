29958e2c4e40ff1d40bfabf90afd2d71551026ac
AgathaLynn <amaskelton@verizon.net>
2017-07-16 19:34:20 -0400

    updated spacing to make linter happy

--- models/users.js
+++ models/users.js
@@ -9,23 +9,23 @@ const User = new Schema({
     email: String,
     image: String,
     team: {
-			name: String,
-			id: String,
-			domain: String,
-			image: String,
-		}
-	},
-	profile: {
-		timezone: Number, // UTC -5 entered as -5
-		fccScore: Number, // Based on FCC levels completed?
-		fccLevel: String
-	},
-	pending: {
-		created: Date
-	},
-	newMatch: Boolean,
-	matchedTo: {type: Schema.Types.ObjectId, ref: 'User'},
-	admin: Boolean
+      name: String,
+      id: String,
+      domain: String,
+      image: String,
+    },
+  },
+  profile: {
+    timezone: Number, // UTC -5 entered as -5
+    fccScore: Number, // Based on FCC levels completed?
+    fccLevel: String,
+  },
+  pending: {
+    created: Date,
+  },
+  newMatch: Boolean,
+  matchedTo: { type: Schema.Types.ObjectId, ref: 'User' },
+  admin: Boolean,
 });
 
 module.exports = mongoose.model('User', User);
