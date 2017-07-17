310810285c7564d971efe5f1a6021f3682a5507d
AgathaLynn <amaskelton@verizon.net>
2017-07-17 14:17:36 -0400

    a bit of tidying up

--- routes/slack/actions.js
+++ routes/slack/actions.js
@@ -6,18 +6,21 @@ const User = require('../../models/users');
 const runMatch = require('../../lib/match_user.js');
 
 router.post('/', function(req, res) {
+  // get and validate form information
   const formData = JSON.parse(req.body.payload);
-
   if (formData.token !== process.env.VERIFICATION_TOKEN) {
     return;
   }
+  // receive: request for form, send: fcc certificate selector
   if (formData.callback_id === 'form_request') {
     res.json(responses.categoryForm());
   }
+  // receive: certificate info, send: sections in certificate
   else if (formData.callback_id === 'category_selection') {
     var category = formData.actions[0].selected_options[0].value;
     res.json(responses.sectionForm(category));
   }
+  // receive: fccScore, send: timezone selector
   else if (formData.callback_id === 'section_selection') {
     User.findOneAndUpdate(
       { 'slack.id': formData.user.id, 'slack.team.id': formData.team.id },
@@ -26,11 +29,12 @@ router.post('/', function(req, res) {
         'profile.fccLevel': fccScore.getLevel(formData.actions[0].selected_options[0].value)
       } }
     )
-    .then(function sendResponse() {
+    .then(function() {
       res.json(responses.timezoneForm());
     })
     .catch(error => console.error(error));
   }
+  // receive: timezone selection, send: form submit button
   else if (formData.callback_id === 'timezone_selection') {
     User.findOneAndUpdate(
       { 'slack.id': formData.user.id, 'slack.team.id': formData.team.id },
@@ -43,8 +47,8 @@ router.post('/', function(req, res) {
       res.json(responses.submitForm(user.profile.fccLevel, user.profile.timezone));
     })
     .catch(error => console.error(error));
-    //res.json({text: 'Great! Your request has been submitted. We\'ll let you know when you\'ve been matched with a pair programming partner!' });
   }
+  // receive: form submit/cancel button, send: confirmation message
   else if (formData.callback_id === 'form_submit') {
     if (formData.actions[0].value === 'submit') {
       User.findOneAndUpdate(
@@ -63,7 +67,10 @@ router.post('/', function(req, res) {
         { 'slack.id': formData.user.id, 'slack.team.id': formData.team.id },
         { $unset: { 'pending.created': '' } }
       )
-      res.json(responses.cancelMessage());
+      .then(function() {
+        res.json(responses.cancelMessage());
+      })
+      .catch(function(error) { console.error(error); });
     }
   }
 });
