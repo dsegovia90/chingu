5211a58fe03d73e09e7e81f5085832813a335a55
AgathaLynn <amaskelton@verizon.net>
2017-07-16 20:46:10 -0400

    changes to please linter

--- routes/request-match.js
+++ routes/request-match.js
@@ -1,36 +1,37 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../models/users');
-var runMatch = require('../lib/match_user.js');
-var fcc = require('../lib/fccScore.js');
+const express = require('express');
+
+const router = express.Router();
+const User = require('../models/users');
+const runMatch = require('../lib/match_user.js');
+const fcc = require('../lib/fccScore.js');
 
 router.get('/', function (req, res) {
-  var fccLevels = fcc.toLevelsArray();
-  res.render('request-match', {fccLevels: fccLevels});
+  const fccLevels = fcc.toLevelsArray();
+  res.render('request-match', { fccLevels });
 });
 
 router.post('/', function (req, res) {
-  var update = {
+  const update = {
     $set: {
       profile: {
         timezone: req.body.timezone,
         fccScore: req.body.fccScore,
-        fccLevel: fcc.getLevel(req.body.fccScore)
+        fccLevel: fcc.getLevel(req.body.fccScore),
       },
       pending: {
-        created: new Date()
-      }
-    }
+        created: new Date(),
+      },
+    },
   };
 
-  User.findByIdAndUpdate(req.user._id, update, {new: true}).exec()
+  User.findByIdAndUpdate(req.user._id, update, { new: true }).exec()
   .then(runMatch);
 
   res.redirect('/');
 });
 
 router.get('/delete', function (req, res) {
-  User.findByIdAndUpdate(req.user._id, { $unset: { pending: "" } }).exec();
+  User.findByIdAndUpdate(req.user._id, { $unset: { pending: '' } }).exec();
   res.redirect('/');
 });
 
