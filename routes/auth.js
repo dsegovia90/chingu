e4121498d04a58e44cdc9205fe69fdeb0d48e79f
Daniel Segovia <segoarce90@gmail.com>
2017-07-27 13:06:54 -0500

    refactored to simplify message

--- routes/auth.js
+++ routes/auth.js
@@ -28,43 +28,25 @@ router.get('/slack/install', function (req, res) {
     }
     Team.findOne({accessToken: data.access_token})
     .then(function(team){
-      if(team){
-        // Team exists already
-        team.accessToken = data.access_token;
-        team.scope = data.scope;
-        team.userId = data.user_id;
-        team.teamName = data.team_name;
-        team.teamId = data.team_id;
-        team.bot = {
-          bot_user_id: data.bot.bot_user_id,
-          bot_access_token: data.bot.bot_access_token
-        }
-        team.save(function(err){
-          if(err) {
-            throw err
-          }else{
-            //send app already installed and success update message
-          }
-        })
-      }else{
-        var newTeam = new Team()
-        newTeam.accessToken = data.access_token;
-        newTeam.scope = data.scope;
-        newTeam.userId = data.user_id;
-        newTeam.teamName = data.team_name;
-        newTeam.teamId = data.team_id;
-        newTeam.bot = {
-          bot_user_id: data.bot.bot_user_id,
-          bot_access_token: data.bot.bot_access_token
-        }
-        newTeam.save(function(err){
-          if(err) {
-            throw err;
-          }else{
-            //send success installation message.
-          }
-        })
+      if(!team){
+        team = new Team(); // Team didn't exist.
       }
+      team.accessToken = data.access_token;
+      team.scope = data.scope;
+      team.userId = data.user_id;
+      team.teamName = data.team_name;
+      team.teamId = data.team_id;
+      team.bot = {
+        bot_user_id: data.bot.bot_user_id,
+        bot_access_token: data.bot.bot_access_token
+      }
+      team.save(function(err){
+        if(err) {
+          throw err
+        }else{
+          //send app already installed and success update message
+        }
+      })
       res.redirect('/')
     })
   })
